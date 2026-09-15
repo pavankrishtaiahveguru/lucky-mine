cecf6539e7a6199c9ccb31b97b30e0d9f659d121
pavankrishtaiahveguru <pavankrishtaiah@gmail.com>
2026-09-15 18:56:28 +0530

    update fav icon

--- src/app/layout.js
+++ src/app/layout.js
@@ -19,6 +19,11 @@ export const metadata = {
   title: "Lucky Mine",
   description:
     "Shop everyday essentials across Health & Household, Beauty & Personal Care, Sports & Outdoor, Grocery, Art & Craft, and Tools.",
+  icons: {
+    icon: "/logo.png",
+    shortcut: "/logo.png",
+    apple: "/logo.png",
+  },
 };
 
 export default function RootLayout({ children }) {
